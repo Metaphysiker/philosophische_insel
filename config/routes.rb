2365b70f02026a824f225d62a7bc7a9f60a8a7b1
Metaphysiker <s.raess@me.com>
2021-05-13 20:23:02 +0200

    working on chat

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'chat/chat'
   root 'static_pages#welcome'
   get 'static_pages/welcome'
+
+  get 'chat/select_option(/:id)', to: 'chat#select_option', as: 'chat_select_option'
+  get '/debate_a_vegan(/:slide)', to: 'debate_a_vegan#debate', as: 'debate_a_vegan'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
