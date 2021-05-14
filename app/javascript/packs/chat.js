f2f4fa110c1e86e53b0b3b6802d841e2bd96d36e
Metaphysiker <s.raess@me.com>
2021-05-14 12:55:58 +0200

    added transform to button

--- app/javascript/packs/chat.js
+++ app/javascript/packs/chat.js
@@ -60,6 +60,7 @@ window.CHAT = (function() {
             //$(element).empty();
           });
         },
+        loading_time_for_chat_message: 1800,
         animateCSS: function(element, animation) {
           animateCSS(element, animation)
         }
