411d7ba5b9afd4b405bc60ca7f45b322ee2fa485
Metaphysiker <s.raess@me.com>
2021-05-15 20:35:18 +0200

    working on chat

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -4,9 +4,16 @@ class ChatController < ApplicationController
     render layout: "application_chat"
   end
 
-  def select_option
+  def get_next_chat_message
     if params[:id].present?
       @chat_message = ChatMessage.find(params[:id])
     end
   end
+
+  def get_next_chat_button
+    if params[:id].present?
+      @chat_message = ChatMessage.find(params[:id])
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root 'static_pages#welcome'
   get 'static_pages/welcome'
 
-  get 'chat/select_option(/:id)', to: 'chat#select_option', as: 'chat_select_option'
+  get 'chat/get_next_chat_message(/:id)', to: 'chat#get_next_chat_message', as: 'get_next_chat_message'
+  get 'chat/get_next_chat_button(/:id)', to: 'chat#get_next_chat_button', as: 'get_next_chat_button'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
