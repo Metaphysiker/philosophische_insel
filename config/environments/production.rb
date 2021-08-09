0822e7a9fc4130c0e15de5f08ae8e9ab52c31e5c
Metaphysiker <s.raess@me.com>
2021-08-09 12:25:35 +0200

    added hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "www.philosophische-insel.ch"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
