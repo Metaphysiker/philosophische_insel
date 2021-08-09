37ed8f6164f63d304be280f728534b9b7b2cb249
Metaphysiker <s.raess@me.com>
2021-08-09 19:26:53 +0200

    changed assets.compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+
+  #I CHANGED THAT
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
--- drop-create-migrate-seed.rb
+++ drop-create-migrate-seed.rb
@@ -1,1 +1,2 @@
 rails db:drop && rails db:create && rails db:migrate && rails db:seed
+rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1 && rails db:create && rails db:migrate && rails db:seed
