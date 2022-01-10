727a41b70537633e045f7af40dfe435754ece879
Metaphysiker <s.raess@me.com>
2022-01-10 17:14:53 +0100

    working on cockpit

--- app/javascript/packs/d3-charts.js
+++ app/javascript/packs/d3-charts.js
@@ -1,15 +1,9 @@
-console.log("d3-charts");
-
-export function AreaChart(container_class) {
-   this.container_class = container_class;
-   this.log = function() {
-     console.log(this.container_class);
-   };
- }
-
  export function HorizontalBarChart(container_class, data) {
    this.container_class = container_class;
    this.data = data;
+   this.empty_container = function(){
+     $(this.container_class).empty();
+   }
    this.get_max_value_of_array_with_hashes = function(data){
      var array_with_values = [];
      for (var i = 0; i < data.length; i++) {
@@ -20,6 +14,8 @@ export function AreaChart(container_class) {
 
    },
    this.draw_chart = function() {
+
+     this.empty_container();
 
      var margin = {top: 50, right: 50, bottom: 50, left: 50};
      var width = $(container_class).width() - margin.left - margin.right;
@@ -63,7 +59,7 @@ export function AreaChart(container_class) {
           .attr("height", y.bandwidth())
           .attr("fill", "#00c39b");
 
-          //add Text to the bars
+          //add name above the bars
           svg.append("g")
               .attr("fill", "#2c4959")
               .attr("font-family", "sans-serif")
@@ -76,6 +72,102 @@ export function AreaChart(container_class) {
               .attr("dy", "0.35em")
               .attr("dx", -4)
               .text(d => d.name);
+
+          //add value inside the bars
+          svg.append("g")
+              .attr("fill", "#2c4959")
+              .attr("font-family", "sans-serif")
+            .selectAll("text")
+            .data(data)
+            .join("text")
+              //.attr("x", d => x(d.value))
+              .attr("x", d => x(0) + 10)
+              .attr("y", d => y(d.name) + y.bandwidth()/2)
+              .attr("dy", "0.35em")
+              .attr("dx", -4)
+              .text(d => d.value);
+
+   };
+
+ }
+
+ export function VerticalBarChart(container_class, data) {
+   this.container_class = container_class;
+   this.data = data;
+   this.empty_container = function(){
+     $(this.container_class).empty();
+   }
+   this.get_max_value_of_array_with_hashes = function(data){
+     var array_with_values = [];
+     for (var i = 0; i < data.length; i++) {
+       array_with_values.push(data[i].value);
+    }
+
+    return Math.max(...array_with_values);
+
+   },
+   this.draw_chart = function() {
+
+     this.empty_container();
+
+     var margin = {top: 50, right: 50, bottom: 50, left: 50};
+     var width = $(container_class).width() - margin.left - margin.right;
+     var height = 500 - margin.top - margin.bottom;
+
+       // append the svg object to the body of the page
+      const svg = d3.select(this.container_class)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+        // Add y axis
+        const y = d3.scaleLinear()
+          .domain([0, this.get_max_value_of_array_with_hashes(data)])
+          .range([ height, 0]);
+
+        svg.append("g")
+          .attr("transform", `translate(0, 0)`)
+          .call(d3.axisLeft(y))
+          .selectAll("text")
+            //.attr("transform", "translate(-10,0)rotate(-45)")
+            .style("text-anchor", "end");
+
+        // x axis
+        const x = d3.scaleBand()
+          .range([ 0, width ])
+          .domain(data.map(d => d.name))
+          .padding(.1);
+
+        svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+        //Bars
+        svg.selectAll("myRect")
+          .data(data)
+          .join("rect")
+          .attr("y", d => y(d.value))
+          .attr("x", d => x(d.name))
+          .attr("width", x.bandwidth())
+          .attr("height", d => height - y(d.value))
+          .attr("fill", "#00c39b");
+
+          //add value at top of the bars
+          svg.append("g")
+              .attr("fill", "#2c4959")
+              .attr("font-family", "sans-serif")
+              .attr("font-size", 12)
+            .selectAll("text")
+            .data(data)
+            .join("text")
+              //.attr("x", d => x(d.value))
+              .attr("x", d => x(d.name))
+              .attr("y", d => y(d.value))
+              .attr("dy", -10)
+              .attr("dx", 5)
+              .text(d => d.value);
 
    };
 
