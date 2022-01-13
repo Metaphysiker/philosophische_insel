6e847e31a4dd91c49f55aa1915d02c843a7a0de2
Metaphysiker <s.raess@me.com>
2022-01-13 18:32:21 +0100

    added regexp

--- app/javascript/packs/vegan_cockpit.js
+++ app/javascript/packs/vegan_cockpit.js
@@ -867,3 +867,239 @@ export function SourceForSinglePageReview(container_class, dateRanges, view_id, 
     })
   }
 }
+
+export function MonthlyRegexpPageReview(container_class, dateRanges, view_id, page_name) {
+  this.container_class = container_class,
+  this.dateRanges = dateRanges,
+  this.view_id = view_id,
+  this.page_name = page_name,
+  this.table_name = this.container_class + "_table",
+  this.svg_container = this.container_class + "_svg_container",
+  this.date_format_options = {  year: 'numeric', month: 'long' },
+  this.generate_report = function() {
+    var container_class = this.container_class;
+    var table_name = this.table_name;
+    var svg_container = this.svg_container;
+    var dateRange = this.dateRange;
+    var view_id = this.view_id;
+    var date_format_options = this.date_format_options;
+    var page_name = this.page_name;
+
+    return new Promise(function(outer_promise_resolve, outer_promise_reject)
+    {
+      var data = [];
+
+      $("." + container_class).empty();
+
+      $("." + container_class).append(`
+        <h2>Monatliche Übersicht für alle Seiten mit "${page_name}"</h2>
+        <table class="${table_name} table table-striped">
+          <thead>
+
+            <tr class="head_tr">
+              <th scope="col">Monat</th>
+              <th scope="col">Pageviews</th>
+              <th scope="col">unique Pageviews</th>
+              <th scope="col">Sessions</th>
+              <th scope="col">Users</th>
+            </tr>
+
+          </thead>
+          <tbody class="tbody">
+
+          </tbody>
+        </table>
+
+        <div class="${svg_container}">
+
+        </div>
+        `);
+
+    //  $("." + table_name + " .tbody").empty();
+
+      for (let main_index = 0, p = Promise.resolve(); main_index < dateRanges.length; main_index++)
+        {
+            p = p.then(() => new Promise(function(resolve, reject) {
+              gapi.client.request({
+                path: '/v4/reports:batchGet',
+                root: 'https://analyticsreporting.googleapis.com/',
+                method: 'POST',
+                body: {
+                  reportRequests: [
+                    {
+                      viewId: VIEW_ID,
+                      dateRanges: [dateRanges[main_index]],
+                      metrics: [
+                        {
+                          expression: 'ga:pageviews'
+                        },
+                        {
+                          expression: 'ga:uniquePageviews'
+                        },
+                        {
+                          expression: 'ga:sessions'
+                        },
+                        {
+                          expression: 'ga:users'
+                        }
+                      ],
+                      "dimensionFilterClauses": [
+                       {
+                        "filters": [
+                        {
+                         "operator": "REGEXP",
+                         "dimensionName": "ga:pagePath",
+                         "expressions": [
+                           page_name
+                          ]
+                        }
+                        ]
+                       }
+                      ]
+                    }
+                  ]
+                }
+              }).then(function(response){
+
+                var metrics = response.result.reports[0].data.rows[0].metrics;
+                for (var i = 0; i < metrics.length; i++) {
+                  var values = metrics[i].values;
+                  $("." + table_name + " .tbody").append(`
+                  <tr>
+                  </tr>
+                  `);
+                  var date_of_current_row = new Date(dateRanges[main_index].startDate);
+                  $("." + table_name + " tr:last").append(`
+                    <td>${date_of_current_row.toLocaleDateString('de-DE', date_format_options)}</td>
+                  `);
+
+                  for (var inner_i = 0; inner_i < values.length; inner_i++) {
+                      $("." + table_name + " tr:last").append(`
+                        <td>${values[inner_i]}</td>
+                      `);
+                  }
+
+                  data.push({name: date_of_current_row.toLocaleDateString('de-DE', {  year: '2-digit', month: 'short' }), value: values[3]});
+                }
+
+                resolve("done");
+                if(main_index + 1 >= dateRanges.length){
+
+                  var vertical_bar_chart_grouped_two = new d3Charts.VerticalBarChart("." + svg_container, data);
+                  vertical_bar_chart_grouped_two.draw_chart();
+
+                  outer_promise_resolve("this report is done");
+                  //outer_promise_reject("error");
+                }
+              }, function(error){
+                outer_promise_reject("error");
+              })
+            } // end of inner promise
+        ));
+      } // end of for loop
+    })
+  }
+}
+
+export function SourceForRegexpPageReview(container_class, dateRanges, view_id, page_name) {
+  this.container_class = container_class,
+  this.dateRanges = dateRanges,
+  this.view_id = view_id,
+  this.page_name = page_name,
+  this.table_name = this.container_class + "_table",
+  this.svg_container = this.container_class + "_svg_container",
+  this.date_format_options = {  year: 'numeric', month: 'long' },
+  this.generate_report = function() {
+    var container_class = this.container_class;
+    var table_name = this.table_name;
+    var svg_container = this.svg_container;
+    var dateRange = this.dateRange;
+    var view_id = this.view_id;
+    var date_format_options = this.date_format_options;
+    var page_name = this.page_name;
+
+    return new Promise(function(outer_promise_resolve, outer_promise_reject)
+    {
+      var data = [];
+
+      $("." + container_class).empty();
+
+      $("." + container_class).append(`
+        <h2>Quellen-Übersicht für alle Seiten mit ${page_name}</h2>
+
+        <div class="${svg_container}">
+
+        </div>
+        `);
+
+    //  $("." + table_name + " .tbody").empty();
+
+              gapi.client.request({
+                path: '/v4/reports:batchGet',
+                root: 'https://analyticsreporting.googleapis.com/',
+                method: 'POST',
+                body: {
+                  reportRequests: [
+                    {
+                      viewId: VIEW_ID,
+                      dateRanges: [{startDate: dateRanges[0].startDate, endDate: dateRanges[dateRanges.length -1].endDate }],
+                      dimensions: [
+                        {
+                          name: "ga:source"
+                        }
+                      ],
+                      metrics: [
+                        {
+                          expression: 'ga:users'
+                        }
+                      ],
+                      "dimensionFilterClauses": [
+                       {
+                        "filters": [
+                        {
+                         "operator": "REGEXP",
+                         "dimensionName": "ga:pagePath",
+                         "expressions": [
+                           page_name
+                          ]
+                        }
+                        ]
+                       }
+                     ],
+                     "orderBys": [
+                       {"fieldName": "ga:users", "sortOrder": "DESCENDING"},
+                     ]
+                    }
+                  ]
+                }
+              }).then(function(response){
+                var data_of_sources = [];
+                var rows = response.result.reports[0].data.rows;
+
+                for (var i = 0; i < 15; i++) {
+                  var result_hash = {
+                    name: rows[i].dimensions[0],
+                    value: rows[i].metrics[0].values[0]
+                  };
+                  data_of_sources.push(result_hash);
+                }
+
+                $("." + container_class).append(`
+                  <div class="${container_class + "_" + "svg_container"}">
+
+                  </div>
+                  `);
+
+                  var horizontal_bar_chart = new d3Charts.HorizontalBarChart("." + container_class + "_" + "svg_container", data_of_sources);
+                  horizontal_bar_chart.draw_chart();
+
+                outer_promise_resolve("done");
+
+              }, function(error){
+                outer_promise_reject("error");
+              })
+
+
+    })
+  }
+}
