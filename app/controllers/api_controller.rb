cbe7956cdb4fa52316f0543e55ad98387ae17999
Metaphysiker <s.raess@me.com>
2022-01-20 14:19:33 +0100

    added headers

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -21,10 +21,14 @@ class ApiController < ApplicationController
   end
 
   def vegan_cockpit_js
+    response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM https://vegan.ch/"
+
     render file: "#{Rails.root}/app/javascript/packs/vegan_cockpit.js", layout: false
   end
 
   def d3_charts_js
+    response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM https://vegan.ch/"
+
     render file: "#{Rails.root}/app/javascript/packs/d3-charts.js", layout: false
   end
 
