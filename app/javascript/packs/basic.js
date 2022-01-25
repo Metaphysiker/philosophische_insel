8d7232b1fdd403b680e71b5e989252a2599c3e14
Metaphysiker <s.raess@me.com>
2022-01-25 08:18:29 +0100

    removed js from welcome and about

---
+++ app/javascript/packs/basic.js
@@ -0,0 +1,7 @@
+import Rails from "@rails/ujs"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "font-awesome/scss/font-awesome.scss"
+
+Rails.start()
+ActiveStorage.start()
