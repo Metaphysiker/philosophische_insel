e920f1700d35d324e6d8b294a7470c325f095bb6
Metaphysiker <s.raess@me.com>
2022-01-26 12:09:38 +0100

    added dotted and removed jquery

---
+++ public/packs/js/google_analytics-d9e01f14e1c476aec0dd.chunk.js
@@ -0,0 +1,108 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["google_analytics"],{
+
+/***/ "./app/javascript/channels sync recursive _channel\\.js$":
+/*!****************************************************!*\
+  !*** ./app/javascript/channels sync _channel\.js$ ***!
+  \****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	var e = new Error("Cannot find module '" + req + "'");
+	e.code = 'MODULE_NOT_FOUND';
+	throw e;
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = "./app/javascript/channels sync recursive _channel\\.js$";
+
+/***/ }),
+
+/***/ "./app/javascript/channels/index.js":
+/*!******************************************!*\
+  !*** ./app/javascript/channels/index.js ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// Load all the channels within this directory and all subdirectories.
+// Channel files must be named *_channel.js.
+var channels = __webpack_require__("./app/javascript/channels sync recursive _channel\\.js$");
+
+channels.keys().forEach(channels);
+
+/***/ }),
+
+/***/ "./app/javascript/packs/google_analytics.js":
+/*!**************************************************!*\
+  !*** ./app/javascript/packs/google_analytics.js ***!
+  \**************************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function($) {/* harmony import */ var _rails_ujs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @rails/ujs */ "./node_modules/@rails/ujs/lib/assets/compiled/rails-ujs.js");
+/* harmony import */ var _rails_ujs__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_rails_ujs__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _rails_activestorage__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @rails/activestorage */ "./node_modules/@rails/activestorage/app/assets/javascripts/activestorage.js");
+/* harmony import */ var _rails_activestorage__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_rails_activestorage__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var channels__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! channels */ "./app/javascript/channels/index.js");
+/* harmony import */ var channels__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(channels__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var bootstrap__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.esm.js");
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var font_awesome_scss_font_awesome_scss__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! font-awesome/scss/font-awesome.scss */ "./node_modules/font-awesome/scss/font-awesome.scss");
+/* harmony import */ var font_awesome_scss_font_awesome_scss__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(font_awesome_scss_font_awesome_scss__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var d3_dist_d3_js__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! d3/dist/d3.js */ "./node_modules/d3/dist/d3.js");
+/* harmony import */ var jquery_ui_ui_widgets_datepicker_js__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! jquery-ui/ui/widgets/datepicker.js */ "./node_modules/jquery-ui/ui/widgets/datepicker.js");
+/* harmony import */ var jquery_ui_ui_widgets_datepicker_js__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(jquery_ui_ui_widgets_datepicker_js__WEBPACK_IMPORTED_MODULE_7__);
+/* harmony import */ var jquery_ui_themes_base_all_css__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! jquery-ui/themes/base/all.css */ "./node_modules/jquery-ui/themes/base/all.css");
+/* harmony import */ var jquery_ui_themes_base_all_css__WEBPACK_IMPORTED_MODULE_8___default = /*#__PURE__*/__webpack_require__.n(jquery_ui_themes_base_all_css__WEBPACK_IMPORTED_MODULE_8__);
+/* harmony import */ var bootstrap_icons_font_bootstrap_icons_css__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! bootstrap-icons/font/bootstrap-icons.css */ "./node_modules/bootstrap-icons/font/bootstrap-icons.css");
+/* harmony import */ var bootstrap_icons_font_bootstrap_icons_css__WEBPACK_IMPORTED_MODULE_9___default = /*#__PURE__*/__webpack_require__.n(bootstrap_icons_font_bootstrap_icons_css__WEBPACK_IMPORTED_MODULE_9__);
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+
+
+
+ //import "font-awesome/css/font-awesome.min.css"
+
+ //import { Personyxz } from "packs/d3_objects"
+
+
+
+
+
+_rails_ujs__WEBPACK_IMPORTED_MODULE_0___default.a.start();
+_rails_activestorage__WEBPACK_IMPORTED_MODULE_1__["start"]();
+window.jQuery = $;
+window.$ = $; //import * as d3Charts from "packs/d3-charts";
+//window.d3Charts = d3Charts;
+
+__webpack_require__.e(/*! import() */ 0).then(__webpack_require__.bind(null, /*! packs/d3-charts */ "./app/javascript/packs/d3-charts.js")).then(function (d3Charts) {
+  window.d3Charts = d3Charts;
+});
+__webpack_require__.e(/*! import() */ 1).then(__webpack_require__.bind(null, /*! packs/vegan_cockpit */ "./app/javascript/packs/vegan_cockpit.js")).then(function (veganCockpit) {
+  window.veganCockpit = veganCockpit;
+}); //window.Lazy = Lazy
+//window.d3 = d3
+
+__webpack_require__(/*! jquery-lazy/jquery.lazy.js */ "./node_modules/jquery-lazy/jquery.lazy.js");
+
+$.fn.isInViewport = function () {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
+
+/***/ })
+
+},[["./app/javascript/packs/google_analytics.js","runtime~google_analytics","vendors~application~application_old~basic~chat~d3-charts~google_analytics~pferdefutter~vegan_cockpit~welcome","vendors~application~application_old~basic~google_analytics~pferdefutter~welcome","vendors~application~application_old~basic~google_analytics~pferdefutter","vendors~application~application_old~google_analytics~pferdefutter","vendors~application~application_old~basic~google_analytics","vendors~application~application_old~google_analytics"]]]);
+//# sourceMappingURL=google_analytics-d9e01f14e1c476aec0dd.chunk.js.map
