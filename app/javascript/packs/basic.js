264127a04f2aefa17868b101a34557823689ca27
Metaphysiker <s.raess@me.com>
2022-02-02 14:06:31 +0100

    precompile

--- app/javascript/packs/basic.js
+++ app/javascript/packs/basic.js
@@ -1,13 +1,14 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "font-awesome/scss/font-awesome.scss"
-import "font-awesome/css/font-awesome.css"
 import "bootstrap"
 import "jquery"
 import "jquery-ui/ui/widgets/datepicker.js"
 import "jquery-ui/themes/base/all.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
+import "@fortawesome/fontawesome-free/scss/fontawesome.scss"
+import "@fortawesome/fontawesome-free/js/all.js"
+
 
 Rails.start()
 ActiveStorage.start()
--- public/packs/js/animate_css-f31403144203b080785c.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{16:function(n,e){window.ANIMATECSS=function(n,e){var t=arguments.length>2&&void 0!==arguments[2]?arguments[2]:"animate__";return new Promise((function(a,i){var o="".concat(t).concat(e),c=document.querySelector(n);c.classList.add("".concat(t,"animated"),o),c.addEventListener("animationend",(function n(){c.classList.remove("".concat(t,"animated"),o),c.removeEventListener("animationend",n),a("Animation ended")}))}))}}},[[16,14]]]);
-//# sourceMappingURL=animate_css-f31403144203b080785c.chunk.js.map
