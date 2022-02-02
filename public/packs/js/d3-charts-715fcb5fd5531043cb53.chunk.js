812fcbb138a756b34bf48694b4d372002a85e037
Metaphysiker <s.raess@me.com>
2022-02-02 19:54:55 +0100

    changed f-color

---
+++ public/packs/js/d3-charts-715fcb5fd5531043cb53.chunk.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["d3-charts"],[],[["./app/javascript/packs/d3-charts.js","runtime~d3-charts","vendors~application~application_old~basic~chat~d3-charts~google_analytics~old_chat~pferdefutter~vega~6725d034",0]]]);
+//# sourceMappingURL=d3-charts-715fcb5fd5531043cb53.chunk.js.map
