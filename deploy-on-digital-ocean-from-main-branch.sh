affdd59fd2ce46881337beeb878f13c21f34e9ed
Metaphysiker <s.raess@me.com>
2022-02-11 20:23:20 +0100

    precompile

--- deploy-on-digital-ocean-from-main-branch.sh
+++ deploy-on-digital-ocean-from-main-branch.sh
@@ -5,12 +5,15 @@ git add .
 git commit -m "precompile"
 git push origin main
 
-ssh root@159.65.120.231 << EOF
+ssh sandro@159.65.120.231 << EOF
    cd /home/sandro/philosophische_insel/
 
    git pull origin main
    bundle config set --local without 'development test'
    bundle install
    RAILS_ENV=production rails db:migrate
+EOF
+
+ssh root@159.65.120.231 << EOF
    sudo service nginx restart
 EOF
