01be8423fea0a73621ed649d29e686db8f265fb3
Metaphysiker <s.raess@me.com>
2022-02-11 20:19:15 +0100

    different look

--- deploy-on-digital-ocean-from-main-branch.sh
+++ deploy-on-digital-ocean-from-main-branch.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-ssh sandro@159.65.120.231
-cd philosophische_insel/
+rails assets:clobber
+RAILS_ENV=production rails assets:precompile
+git add .
+git commit -m "precompile"
+git push origin main
 
-git pull origin main
-bundle config set --local without 'development test'
-bundle install
-RAILS_ENV=production rails db:migrate
-sudo service nginx restart
+ssh root@159.65.120.231 << EOF
+   cd /home/sandro/philosophische_insel/
+   cd philosophische_insel/
 
-exit
+   git pull origin main
+   bundle config set --local without 'development test'
+   bundle install
+   RAILS_ENV=production rails db:migrate
+   sudo service nginx restart
+EOF
