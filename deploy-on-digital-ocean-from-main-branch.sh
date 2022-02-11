ce4cae74eae872d0d42679c2a700262d952741fb
Metaphysiker <s.raess@me.com>
2022-02-11 11:03:40 +0100

    precompile

--- deploy-on-digital-ocean-from-main-branch.sh
+++ deploy-on-digital-ocean-from-main-branch.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ssh sandro@159.65.120.231
 cd philosophische_insel/
+
 git pull origin main
 bundle config set --local without 'development test'
 bundle install
 RAILS_ENV=production rails db:migrate
 sudo service nginx restart
+
 exit
