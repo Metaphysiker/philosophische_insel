96d92e8adf9f779df117a31bfb113d0ef7d07d51
Metaphysiker <s.raess@me.com>
2022-03-16 07:26:04 +0100

    added possible solution to cheat sheet

--- cheat-sheet.rb
+++ cheat-sheet.rb
@@ -5,3 +5,8 @@ rails db:seed:dump MODELS=MediaRelease File=db/seeds/media_releases.rb
 #media seed dump
 
 rails db:seed:dump MODELS=media File=db/seeds/media.rb
+
+
+#when manifest is lacking
+
+RAILS_ENV=production rails webpacker:compile
