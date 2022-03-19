e6765810972aa79b2edd3de619f8eaff723a9ad2
Metaphysiker <s.raess@me.com>
2022-03-19 21:06:44 +0100

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,4 +3,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'https://vegan.ch'
     resource '/api/vegan_visit', headers: :any, methods: [:get, :post, :patch, :put, :options]
   end
+
+  allow do
+    origins '*'
+    resource '/api/vegipass_offers', headers: :any, methods: [:get]
+  end
 end
