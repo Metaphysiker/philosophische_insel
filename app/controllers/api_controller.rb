d347f3be62b7d389a3b432e16c0dff7911bc50b7
Metaphysiker <s.raess@me.com>
2022-03-22 17:59:32 +0100

    added image_url

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -39,17 +39,20 @@ class ApiController < ApplicationController
       {
         company: "Affechrut",
         content: "Bombastische Gewürze und pflanzliche Fleischalternativen – das ist der Beitrag von Affechrut, damit du etwas für deine Gesundheit, den Klimawandel, die Schonung der natürlichen Ressourcen und den Tierschutz tun kannst, ohne auf Genuss verzichten zu müssen.",
-        offer: "25% Rabatt auf eine Bestellung im Onlineshop"
+        offer: "25% Rabatt auf eine Bestellung im Onlineshop",
+        image_url: ActionController::Base.helpers.image_url("affechrut.webp")
       },
       {
         company: "ENGEL-TOFU",
         content: "Engel-Tofu kann sich die älteste Tofuproduktionsstätte der Deutschschweiz nennen und garantiert durch die traditionelle, schonende handwerkliche Fertigung die einzigartige Engel-Tofu Qualität. In ihrem vielfältigen Sortiment, vom Tofu nature, Tofu-Sticks, Tofu-Spiessli über den geräucherten Tofu hin zum Tofu mit Zwiebelsprossen, Soja-Paneer oder Weizen- und Dinkel-Seiten, findest du genau das Richtige um in deine Pfanne zu hauen. Die Genossenschaft hält sich seit 1982 an ihre ethischen und ökologischen Statuten. Überzeuge dich jetzt vom besten Schweizer Tofu!",
-        offer: "10.- Rabatt ab einem Einkauf von 20.- im Online-Shop"
+        offer: "10.- Rabatt ab einem Einkauf von 20.- im Online-Shop",
+        image_url: ActionController::Base.helpers.image_url("engeltofu.jpg")
       },
       {
         company: "SESH",
         content: "Bei Sesh findest du leckere Bowls und eine grosse Portion Gelassenheit. Mache eine Pause vom aktiven Alltag und lade deine Batterien wieder auf. Sie servieren dir naturbelassene, frische Zutaten, raffiniert zusammengesetzt. Be bowld!",
-        offer: "normaler Gastronomierabatt direkt im Restaurant oder 30% Rabatt auf deine Online Bestellung auf sesh.ch/shop: nur Abholung vor Ort, VegiPass vorzeigen."
+        offer: "normaler Gastronomierabatt direkt im Restaurant oder 30% Rabatt auf deine Online Bestellung auf sesh.ch/shop: nur Abholung vor Ort, VegiPass vorzeigen.",
+        image_url: ActionController::Base.helpers.image_url("sesh.png")
       }
     ]
 
