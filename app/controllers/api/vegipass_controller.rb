c4736a7e76b63c5bfe184169b134df7b04572f85
Metaphysiker <s.raess@me.com>
2022-04-08 22:05:09 +0200

    current_user

--- app/controllers/api/vegipass_controller.rb
+++ app/controllers/api/vegipass_controller.rb
@@ -7,7 +7,7 @@ module Api
     end
 
     def am_i_logged_in
-      answer = {answer: "yes, you are logged in"}
+      answer = {answer: "yes, you are logged in", email: current_user.email}
       render json: answer
     end
 
