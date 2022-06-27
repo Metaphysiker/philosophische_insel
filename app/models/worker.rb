1c136ba9e90a70d50b76ce5e05e6da3ec49f9c05
Metaphysiker <s.raess@me.com>
2022-06-27 19:07:37 +0200

    working on work plan

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,4 +1,9 @@
 class Worker < ApplicationRecord
   has_many :worker_workdays
   has_many :workers, :through => :worker_workdays
+
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
 end
