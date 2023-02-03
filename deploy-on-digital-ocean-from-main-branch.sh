0065d962e9907582a4c85a8db38b788b41cd8759
Metaphysiker <s.raess@me.com>
2023-02-03 21:28:28 +0100

    clobber

--- deploy-on-digital-ocean-from-main-branch.sh
+++ deploy-on-digital-ocean-from-main-branch.sh
@@ -14,6 +14,8 @@ ssh sandro@159.65.120.231 << EOF
   bundle config set --local without 'development test'
   bundle install
   RAILS_ENV=production rails db:migrate
+  RAILS_ENV=production rails assets:clobber
+  RAILS_ENV=production rails assets:precompile
 EOF
 
 ssh root@159.65.120.231 << EOF
